e1bf6d9744bc18f05b6977422346f7a0b53683a3
Jesse Stewart <stewed.jesse@gmail.com>
2018-03-02 11:01:59 -0600

    Created Item object added functionality Created itemize_data function: Takes list of lists and number assigned to k, returns list of Item objects. Created assign_classification function: Assigns classification attribute to list of Item objects.

--- KNN.py
+++ KNN.py
@@ -8,6 +8,32 @@ HW2
 
 import csv
 
+# Number of k-items to compare
+num_k = 3
+
+class Item(object):
+    def __init__(self, contents = None, k = None, nearest_neighbors = None, classification = None):
+        self.contents = contents
+        self.k = k
+        self.nearest_neighbors = nearest_neighbors
+        self.classification = classification
+
+# Takes list of lists and number assigned to k
+# returns list of Item objects
+def itemize_data(data, num_k):
+    item_list = []
+    #create list of items
+    for itr in range(1,len(data)):  #skip first tuple
+        item = Item(data[itr], num_k)
+        item_list.append(item)
+    return item_list
+
+# Assigns classification attribute 
+# to list of Item objects
+def assign_classification(itemized_data):
+    for itr in itemized_data:
+        itr.classification = itr.contents[0]
+
 with open('MNIST_train.csv', newline='', encoding='utf_8') as f:
     reader = csv.reader(f)
     train_data = list(reader)
@@ -16,3 +42,9 @@ with open('MNIST_test.csv', newline='', encoding='utf_8') as f:
     reader = csv.reader(f)
     test_data = list(reader)
 
+test_data = itemize_data(test_data, num_k)
+assign_classification(test_data)
+print("test_data[0].classification: ", test_data[0].classification)
+print("test_data[1].classification: ", test_data[1].classification)
+print("test_data[len(test_data)-1].classification: ", test_data[len(test_data)-1].classification)
+print("test_data[len(test_data)-1].k: ", test_data[len(test_data)-1].k)
