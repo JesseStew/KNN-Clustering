3d82ce6f4c42d95732d9f448a5cb86764ceafb81
Jesse Stewart <stewed.jesse@gmail.com>
2018-03-03 22:50:06 -0600

    Algorithm Working Created assign_nearest_neighbors funtion: assigns nearest neighbors to all items in test data, takes as input list of training data items, list of test data items, and k. Created assign_class_by_nn function: assigns classification to all items in test data, takes as input list of test data items with assigned nearest neighbors.

--- KNN.py
+++ KNN.py
@@ -6,15 +6,18 @@ CSC 535
 HW2
 """
 
+clear = "\n" * 100
+
+from statistics import mode
+import math
 import csv
 
 # Number of k-items to compare
 num_k = 3
 
 class Item(object):
-    def __init__(self, contents = None, k = None, nearest_neighbors = None, classification = None):
+    def __init__(self, contents = None, nearest_neighbors = None, classification = None):
         self.contents = contents
-        self.k = k
         self.nearest_neighbors = nearest_neighbors
         self.classification = classification
 
@@ -32,8 +35,56 @@ def itemize_data(data, num_k):
 # to list of Item objects
 def assign_classification(itemized_data):
     for itr in itemized_data:
-        itr.classification = itr.contents[0]
+        itr.classification = itr.contents.pop(0)
 
+# Find euclidean distance measure of two Item objects
+# Input: two lists of numbers which are the same size.
+# Returns: euclidean distance of two lists.
+def euclidean_dist(train_item_data, test_item_data):
+    #print("train_item_data[0]: ", train_item_data[0])
+    squared_euclidean = []
+    euclidean_dist = 0
+    if len(train_item_data) != len(test_item_data):
+        raise NameError("The len(train_item_data) != len(test_item)\nlen(train_item_data): ", len(train_item_data), "len(test_item): ", len(test_item_data))
+    else:
+        for num in range(0,len(train_item_data)):
+            train_i = int(train_item_data[num])
+            test_i = int(test_item_data[num])
+            squared_euclidean.append((train_i-test_i)*(train_i-test_i))
+        for num in squared_euclidean:
+            euclidean_dist = euclidean_dist + num
+        return math.sqrt(euclidean_dist)
+
+# Assigns nearest neighbors to all items in test_data
+# Input: list of training data items, list of test data items, and k
+# No output: assign item.nearest_neighbors internally
+def assign_nearest_neighbors(train_data, test_data, num_k):
+    for num in range(len(test_data)):
+        test_data[num].nearest_neighbors = []
+        for itr in train_data:
+            if len(test_data[num].nearest_neighbors) < num_k:
+                test_data[num].nearest_neighbors.append(itr)
+            else:
+                for item in test_data[num].nearest_neighbors:
+                    t = test_data[num].contents
+                    d = itr.contents
+                    u = item.contents
+                    sim_t_u = euclidean_dist(t, u)
+                    sim_t_d = euclidean_dist(t, d)
+                    if sim_t_u <= sim_t_d:
+                        test_data[num].nearest_neighbors.remove(item)
+                        test_data[num].nearest_neighbors.append(itr)
+
+# Assigns classification to all items in test_data
+# Input: list of test data items
+# No output: assign item.classification internally
+def assign_class_by_nn(test_data):
+    for item in test_data:
+        classification = []
+        for itr in item.nearest_neighbors:
+            classification.append(itr.classification)
+        item.classification = mode(classification)
+        
 with open('MNIST_train.csv', newline='', encoding='utf_8') as f:
     reader = csv.reader(f)
     train_data = list(reader)
@@ -47,4 +98,14 @@ assign_classification(test_data)
 print("test_data[0].classification: ", test_data[0].classification)
 print("test_data[1].classification: ", test_data[1].classification)
 print("test_data[len(test_data)-1].classification: ", test_data[len(test_data)-1].classification)
-print("test_data[len(test_data)-1].k: ", test_data[len(test_data)-1].k)
+
+train_data = itemize_data(train_data, num_k)
+assign_classification(train_data)
+print("\ntrain_data[0].classification: ", train_data[0].classification)
+print("train_data[1].classification: ", train_data[1].classification)
+print("train_data[len(train_data)-1].classification: ", train_data[len(train_data)-1].classification)
+
+assign_nearest_neighbors(train_data, test_data, num_k)
+assign_class_by_nn(test_data)
+
+            
